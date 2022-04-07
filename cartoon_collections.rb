3ad40fc7ff4dbcb6a7f3e2c60631e48cf5d61bad
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-07 18:17:28 -0400

    commit tree 81daf7817cf6f3aee2e76d87ce21a9dd70d7e68c

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves dwarf_names
+  dwarf_names.each_with_index {|name, index| puts "#{index + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map {|cal| cal.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  res = case   calls.find {|cal| cal.length > 4}
+  when nil then false
+  else true
+  end
+  res
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese snacks
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+ chees =   snacks.find {|snack| cheese_types.include?(snack)}
 end
+
